2dd5682343c76fb23442875716cd46c2f98a6fe4
Kim Jinseo <kiy2344@naver.com>
2021-12-09 20:00:54 +0900

    linked_list

--- programmers.py
+++ programmers.py
@@ -227,93 +227,32 @@
 #         stack_num = int(sys.stdin.readline())
 #     stack.append(stack_num)
 
-import datetime
-import requests
-from bs4 import BeautifulSoup
-import telegram
-from apscheduler.schedulers.blocking import BlockingScheduler
+class Node: # 노드의 생성문 
+    def __init__(self,data,next = None):
+        self.data = data
+        self.next = next
+
+class main: # 메인 메서드 동작 및 Head / Tail 생성문
+    def __init__(self) -> None:
+        self.head = None
+        self.tail = None
+
+    def first_insert(self,data):
+        new_node = Node(data)
+        if self.head is None:
+            pass
+        else:
+            pass
+
+    def select_insert(self,data, Node):
+        pass
+
+    def remove(self,data):
+        pass
+
+    def print_node():
+        pass
 
 
-def check_2d():
-    # CGV 메인 도메인 + 예매시간표 페이지 iframe 내 자원주소(src)
-    url = "http://www.cgv.co.kr/common/showtimes/iframeTheater.aspx?areacode=202&theatercode=0325&date=20211214"
-
-    response = requests.get(url)
-    bs = BeautifulSoup(response.text, 'html.parser')
-
-    chatbot = telegram.Bot(token='5048329112:AAF4lvw_NfPr1zV74eD6ZtYD9yd6gHXYAMw')
-
-
-    result = []
-
-    # 이상한 값이 끼어들어와서 이후에 replace로 날려줄 값
-    nullvalue = '[<strong>\r\n                                                '
-    nullvalue2 = '</strong>]'
-
-    # 상영목록이 담긴 리스트를 받아옴
-    two_d = bs.find_all('div', attrs={"class": "col-times"})
-
-    if (two_d):
-        for i in two_d:
-            # 4dx 클래스값을 가진 항목이 있는지 검사
-            if (i.find(class_='info-movie')):
-                # 해당 항목의 a > strong(타이틀부분) 가져옴
-                title = i.select('a > strong')
-                result.append(str(title))
-
-        result = [word.replace(nullvalue, '') for word in result]
-        result = [word.replace(nullvalue2, '') for word in result]
-        for i,j in enumerate(result):
-            if j.find('듄') == -1:
-                result[i] = 0
-            else:
-                chatbot.sendMessage(chat_id=5064622110,
-                                    text=j + " 의 예매가 오픈되었습니다.")
-                # sc.pause()        
-
-    else:
-        chatbot.sendMessage(chat_id=5064622110, text="아직 오픈된 예매가 없습니다.")
-
-
-# 스케쥴 구성을 위한 수행부
-sc = BlockingScheduler()
-sc.add_job(check_2d, 'interval', seconds=60)
-sc.start()
-
-
-
-
-
-
-
-
-
-
-
-
-        
-# def ex4():
-#     result = []
-#     bs = BeautifulSoup(response.text, 'html.parser')
-#     # CSS 처럼 셀렉터를 지정할 수 있다.
-#     # tag = bs.find('div', attrs={'class': 'info-movie'})
-#     tag = bs.find_all('div', attrs={"class": "col-times"})
-#     if (tag):
-#         for i in tag:
-#             # 4dx 클래스값을 가진 항목이 있는지 검사
-#             if (i.find(class_='info-movie')):
-#                 # 해당 항목의 a > strong(타이틀부분) 가져옴
-#                 title = i.select('a > strong')
-#                 result.append(str(title))
-#                 print(result)
-#     # <a href="/movie/bi/mi/basic.nhn?code=161242" title="범죄도시">범죄도시</a>
-
-
-# url = "http://www.cgv.co.kr/common/showtimes/iframeTheater.aspx?areacode=202&theatercode=0325&date="
-# today = datetime.date.today().strftime("%Y%m%d")
-# url += today
-
-# response = requests.get(url)
-# # print(response.text)
-# ex4()
-
+if __name__ == '__main__':
+    test = main()

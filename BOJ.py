9b0c52d54648e41acc3448a302979702a4537395
Kim Jinseo <kiy2344@naver.com>
2022-01-28 15:49:38 +0900

    [Update] All BOJ Success

--- BOJ.py
+++ BOJ.py
@@ -378,11 +378,35 @@
 # BOJ 11650 좌표 정렬하기
 # 2차원 평면 위의 점 N개가 주어진다. 좌표를 x좌표가 증가하는 순으로, x좌표가 같으면 y좌표가 증가하는 순서로 
 # 정렬한 다음 출력하는 프로그램을 작성하시오.
+# import sys
+
+# result = []
+# for i in range(int(input())):
+#     result.append(list(map(int,sys.stdin.readline().split())))
+
+# result.sort()
+
+# for i in result:
+#     print(*i)
+
+
+
+
+# BOJ 10825 국영수
+# 도현이네 반 학생 N명의 이름과 국어, 영어, 수학 점수가 주어진다. 이때, 다음과 같은 조건으로 학생의 성적을 정렬하는 프로그램을 작성하시오.
+# 1. 국어 점수가 감소하는 순서로
+# 2. 국어 점수가 같으면 영어 점수가 증가하는 순서로
+# 3. 국어 점수와 영어 점수가 같으면 수학 점수가 감소하는 순서로
+# 4. 모든 점수가 같으면 이름이 사전 순으로 증가하는 순서로(단, 아스키 코드에서 대문자는 소문자보다 작으므로 사전순으로 앞에 온다.)
+
 import sys
 
-result = []
-for i in range(int(input())):
-    result.append(list(map(int,sys.stdin.readline().split())))
+do_class =[]
+for _ in range(int(input())):
+    result = [int(j) if i > 0 else j for i,j in enumerate(sys.stdin.readline().split())]
+    do_class.append(result)
 
-result.sort()
-print(*result)
+do_class.sort(key = lambda x: (-x[1], x[2], -x[3], x[0]))
+
+for i in do_class:
+    print(i[0])

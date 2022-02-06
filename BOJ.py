19ba8b3f5f67d07110ccf23b1385a4003c78d844
JinseoKim <kiy2344@naver.com>
2022-02-06 21:24:35 +0900

    [Update] BOJ 2164 Success & BOJ 1966 in progress

--- BOJ.py
+++ BOJ.py
@@ -552,31 +552,87 @@
 # 이때, 스택에 push하는 순서는 반드시 오름차순을 지키도록 한다고 하자. 
 # 임의의 수열이 주어졌을 때 스택을 이용해 그 수열을 만들 수 있는지 없는지, 있다면 어떤 순서로 push와 pop 연산을 수행해야 하는지를 알아낼 수 있다.
 # 첫 줄에 n (1 ≤ n ≤ 100,000)이 주어진다. 둘째 줄부터 n개의 줄에는 수열을 이루는 1이상 n이하의 정수가 하나씩 순서대로 주어진다. 물론 같은 정수가 두 번 나오는 일은 없다.
-import sys
 
-n = int(input())
-push_num = int(input())
-cnt = 0
-stack = []
-result = []
+# 문제 초기 생각
+# main() - while문 구성 
+# 수를 하나씩 넣어가며 스택 top에 있는 수와 동일한 지 비교.
+# 동일하다면 - 출력 ㅇㅇ
+# 동일하지 않다면 + 출력 ㅇㅇ
 
-while True:
-    if len(stack) > 0 and (push_num == stack[-1]):
-        stack.pop()
-        result.append('-')
-        if cnt == n and len(stack) == 0:
-            break
-        push_num = int(sys.stdin.readline())
-    else:
-        cnt += 1
-        stack.append(cnt)
-        result.append('+')
+# 기본 변수
+# 함수 밖 input() 초기 변수를 주고 pop의 순간에 input()을 다시 새로 받아 해당 변수의 값을 초기화.
+# 값을 증감해가며 스택에 저장해 줄 변수 cnt 필요. cnt = 1  / cnt += 1
 
-    if len(stack) > 0 and (stack[-1] > push_num):
-        result.append('NO')
-        print('NO')
-        break
+# 불가능(NO)의 경우를 출력하기 위한 if조건문 구성
+# top에 있는 값보다 작을 경우.
+# import sys
 
-if result[-1] != 'NO':
-    for i in result:
-        print(i)
+# n = int(input())
+# push_num = int(input())
+# cnt = 0
+# stack = []
+# result = []
+
+# while True:
+#     if len(stack) > 0 and (push_num == stack[-1]):
+#         stack.pop()
+#         result.append('-')
+#         if cnt == n and len(stack) == 0:
+#             break
+#         push_num = int(sys.stdin.readline())
+#     else:
+#         cnt += 1
+#         stack.append(cnt)
+#         result.append('+')
+
+#     if len(stack) > 0 and (stack[-1] > push_num):
+#         result.append('NO')
+#         print('NO')
+#         break
+
+# if result[-1] != 'NO':
+#     for i in result:
+#         print(i)
+
+
+
+
+# BOJ 2164 카드2
+
+# from collections import deque
+
+# N = int(input())
+# deq = deque(range(1, N+1))
+
+# while True:
+#     if len(deq) == 1:
+#         print(deq[0])
+#         break
+#     deq.popleft()
+#     deq.append(deq.popleft())
+
+
+
+
+# BOJ 1966 프린터 큐
+from collections import deque
+
+for _ in range(int(input())):
+    N, M = map(int,input().split())
+    docs = deque(map(int,input().split()))
+
+    result = []
+    high = docs[0]
+
+    if len(docs) == 1:
+        print(high)
+        continue
+
+    for i in range(1,N):
+        if docs[i] > high:
+            result.append(docs[i])
+            high = docs[i] 
+
+    print(result[-M-1])
+    
+

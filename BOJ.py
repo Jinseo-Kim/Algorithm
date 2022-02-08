d3092327a44ae66b1ecb07334c5eadff267a4f82
JinseoKim <kiy2344@naver.com>
2022-02-09 00:02:59 +0900

    [Update] BOJ 2460 Success

--- BOJ.py
+++ BOJ.py
@@ -731,24 +731,46 @@
 
 # BOJ 15828 Router
 
+# import sys
+
+# N = int(input())
+# queue = []
+# max_size = 0
+
+# while True:
+#     num = int(sys.stdin.readline())
+#     if num == -1:
+#         break
+#     if N > max_size and num != 0:
+#         queue.append(num)
+#         max_size += 1
+#     if num == 0:
+#         queue.pop(0)
+#         max_size -= 1
+
+# if not queue:
+#     print('empty')
+# else:
+#     print(*queue)
+
+
+
+
+# BOJ 2460 지능형 기차2
 import sys
 
-N = int(input())
-queue = []
-max_size = 0
+people = 0
+max = 0
 
-while True:
-    num = int(sys.stdin.readline())
-    if num == -1:
-        break
-    if N > max_size and num != 0:
-        queue.append(num)
-        max_size += 1
-    if num == 0:
-        queue.pop(0)
-        max_size -= 1
+for _ in range(10):
+    off_train, on_train = map(int,sys.stdin.readline().split())
 
-if not queue:
-    print('empty')
-else:
-    print(*queue)
+    if max == 0:
+        max += on_train
+        people += on_train
+    else:
+        people += (on_train - off_train)
+
+    if people > max:
+        max = people
+print(max)

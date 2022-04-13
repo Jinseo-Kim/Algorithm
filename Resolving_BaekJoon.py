3330e19332e7b87f3891326b965da37466012811
Kim Jinseo <kiy2344@naver.com>
2022-04-13 14:34:03 +0900

    [Update] Resolove_problem

--- Resolving_BaekJoon.py
+++ Resolving_BaekJoon.py
@@ -286,5 +286,16 @@ X = int(input())
 line = 0
 max_num = 0
 
-while True:
-    pass
+while X > max_num:
+    line += 1
+    max_num += line
+
+gap = max_num - X
+if line % 2 == 0:
+    top = line - gap
+    bottom = gap + 1
+else:
+    top = gap + 1
+    bottom = line - gap
+
+print(f'{top}/{bottom}')

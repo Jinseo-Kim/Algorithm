c34b9610c28591583fb487780e89a724add5534b
Kim Jinseo <kiy2344@naver.com>
2022-04-14 18:57:12 +0900

    [Update] Resolving_BaekJoon.py(solved problem copy)

--- BaekJoon/Greedy/BOJ 2839 설탕배달.py
+++ BaekJoon/Greedy/BOJ 2839 설탕배달.py
@@ -1,29 +1,17 @@
 N = int(input())
-result = 0
+
+five_kg = N // 5
+remaining_sugar = N % 5
 
 while True:
-    if N % 5 == 0:
-        result += (N // 5)
-        break
-    if N % 3 == 0:
-        result += (N // 3)
-        break
-
-    if N > 3:
-        if N // 5 != 0:
-            N -= 5
-            result += 1
+    if remaining_sugar % 3 > 0:
+        if five_kg <= 0:
+            print(-1)
+            break
+        remaining_sugar += 5
+        five_kg -= 1
     else:
-        result = -1
+        five_kg += (remaining_sugar // 3)
+        print(five_kg)
         break
-
-print(result)
-    
-# 조건을 생각해보자
-# 1. N이 주어진다
-# 2. N을 주어진 5와 3으로 나눈다
-# 2-1. 가장 큰 수로 먼저 나누기를 시도한다.
-# 2-2. 이후 나머지를 3으로 나눈다.
-# 돌지 못하는 조건 = 5로 나눈 나머지가 3보다 작거나 3으로 나눈 나머지가 3보다 작다면 돌지 못한다.
-# 3. 최적의 개수를 구한다.
 

ec7e13d0f76f546f6cb51a7eed0b1c526911ee6d
Kim Jinseo <kiy2344@naver.com>
2022-04-27 19:53:24 +0900

    [Update] draw star

--- Resolving_BaekJoon.py
+++ Resolving_BaekJoon.py
@@ -695,6 +695,7 @@ print(f'{r * r * 2:6f}')
 
 
 ###################################################################### 2022. 04. 26 ################################################################################
+'''
 def fibonacci(n, cnt = 1, prev = 0, current = 1):
     if n == cnt:
         return current
@@ -708,3 +709,37 @@ if n == 0 or n == 1:
     print(n)
 else:
     print(fibonacci(n))
+'''
+###################################################################### 2022. 04. 27 ################################################################################
+def draw_star(n):
+    global Map
+
+    if n == 3:
+        Map[0][:3] = Map[2][:3] = [1]*3
+        Map[1][:3] = [1, 0, 1]
+        return
+
+    a = n//3
+    draw_star(n//3)
+    for i in range(3):
+        for j in range(3):
+            if i == 1 and j == 1:
+                continue
+            for k in range(a):
+                Map[a*i+k][a*j:a*(j+1)] = Map[k][:a]  # 핵심 아이디어
+
+
+N = int(input())
+
+# 메인 데이터 선언
+Map = [[0 for i in range(N)] for i in range(N)]
+
+draw_star(N)
+
+for i in Map:
+    for j in i:
+        if j:
+            print('*', end='')
+        else:
+            print(' ', end='')
+    print()

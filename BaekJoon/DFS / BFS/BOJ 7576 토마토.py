466ad660d85d9ca4a2992961525bd3b2b3c4a5da
Jinseo-Kim <kiy2344@naver.com>
2022-09-07 00:46:10 +0900

    [Complete] 1주일 후 다시 풀어보기 & 스스로 해결 실패 (생각 시간 - 2h)

--- BaekJoon/DFS / BFS/BOJ 7576 토마토.py
+++ BaekJoon/DFS / BFS/BOJ 7576 토마토.py
@@ -1,17 +1,37 @@
 from collections import deque
 import sys
 
-def bfs(tomato_box, ripe_tomato, m, n, dx, dy):
-    queue = deque()
+def bfs(tomato_box, ripe_tomato, m, n, dx, dy, result = -1):
+    while ripe_tomato:
+        for _ in range(len(ripe_tomato)):
+            y, x = ripe_tomato.popleft()
 
+            for i in range(4):
+                width = x + dx[i]
+                height = y + dy[i]
+                if 0 <= height < n and 0 <= width < m:
+                    if tomato_box[height][width] == 0:
+                        tomato_box[height][width] = 1
+                        ripe_tomato.append([height,width])
+
+        result += 1
+    
+    return result
 
 m, n = map(int, input().split())
 tomato_box = [list(map(int,sys.stdin.readline().split())) for _ in range(n)]
 dx, dy = [-1,1,0,0], [0,0,1,-1]     #dx = direction_x, dy = direction_y
-ripe_tomato = []
+ripe_tomato = deque()
 for y in range(n):
     for x in range(m):
         if tomato_box[y][x] == 1:
             ripe_tomato.append([y,x])
 
-bfs(tomato_box, ripe_tomato, m, n, dx, dy)
+result = (bfs(tomato_box, ripe_tomato, m, n, dx, dy))
+
+for tomatoes in tomato_box:
+    for tomato in tomatoes:
+        if tomato == 0:
+            result = -1
+
+print(result)

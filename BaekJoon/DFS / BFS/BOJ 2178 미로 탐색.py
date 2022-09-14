9e044d82d9f24e4c5b0380d24ee6888534b7ccc7
Jinseo-Kim <kiy2344@naver.com>
2022-09-14 21:39:43 +0900

    [ADD & Complete] 미로 탐색 풀이 완료

---
+++ BaekJoon/DFS / BFS/BOJ 2178 미로 탐색.py
@@ -0,0 +1,28 @@
+from collections import deque
+import sys
+
+
+def bfs(n, m, maze, count = 0):
+    queue = deque([[1,1]])
+    move_y, move_x = [-1,1,0,0], [0,0,-1,1]
+    while queue:
+        count += 1
+        for _ in range(len(queue)):
+            coordinate = queue.popleft()
+            if coordinate[0] == n and coordinate[1] == m:
+                return count
+            maze[coordinate[0]][coordinate[1]] = '2'
+
+            for direction in range(4):
+                next_y = coordinate[0] + move_y[direction]
+                next_x = coordinate[1] + move_x[direction]
+                if 0 < next_x <= m and 0 < next_y <= n:
+                    if maze[next_y][next_x] == '1':
+                        queue.append([next_y, next_x])
+                        maze[next_y][next_x] = '2'
+
+
+
+n, m = map(int,input().split())
+maze = [[False]*m] + [[False] + list(input()) for _ in range(n)]
+print(bfs(n, m, maze))

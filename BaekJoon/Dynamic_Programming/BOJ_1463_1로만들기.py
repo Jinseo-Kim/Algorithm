922546df70cc078f904daf8d51d573b923d24614
Kim Jinseo <kiy2344@naver.com>
2023-01-05 19:16:58 +0900

    [Update] Complete

--- BaekJoon/Dynamic_Programming/BOJ_1463_1로만들기.py
+++ BaekJoon/Dynamic_Programming/BOJ_1463_1로만들기.py
@@ -9,13 +9,17 @@
 n = int(input())
 dp_table = [0, 0, 1, 1] # 0~3의 수를 1로 만들기까지 필요한 최소 연산 횟수
 
-for i in range(4, n+1):
-    dp_table.append(dp_table[i-1]+1)
+if n == 1:
+    print(0)
 
-    if dp_table[i] % 3 == 0:
-        dp_table[i] = min(dp_table[i], dp_table[i//3]+1)
+if n != 1:    
+    for i in range(4, n+1):
+        dp_table.append(dp_table[i-1]+1)
+
+        if i % 3 == 0:
+            dp_table[i] = min(dp_table[i], dp_table[i//3]+1)
+        
+        if i % 2 == 0:
+            dp_table[i] = min(dp_table[i], dp_table[i//2]+1)
     
-    if dp_table[i] % 2 == 0:
-        dp_table[i] = min(dp_table[i], dp_table[i//2]+1)
-
-print(dp_table)
+    print(dp_table[-1])
